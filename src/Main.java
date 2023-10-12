0223f84037b97916cc245376c87ad44252b48ac5
unknown <jacob.stemmer@student.htldornbirn.at>
2023-10-12 15:42:45 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+
+        System.out.println(randomNumber);
+
+        if (randomNumber < 20) {
+            System.out.println("Mini");
+        }
+        if ((randomNumber > 20) && (randomNumber < 50)) {
+            System.out.println("Medium");
+        }
+        if (randomNumber > 50) {
+            System.out.println("Large");
+        }
+    }
+}
