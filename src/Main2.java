020f71bddc8acb2130ec6cf3364a42abe41b3839
Jacob <jacob.stemmer@student.htldornbirn.at>
2023-10-19 15:27:44 +0200

    WOOOOWOOWWW

---
+++ src/Main2.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(5,10);
+switch(randomNumber){
+    case 10:
+        System.out.println("ten");
+        break;
+    case 9:
+        System.out.println("nine");
+        break;
+    case 8:
+        System.out.println("eight");
+        break;
+    case 7:
+        System.out.println("seven");
+        break;
+    case 6:
+        System.out.println("six");
+        break;
+    case 5:
+        System.out.println("five");
+        break;
+}
+        }
+}
