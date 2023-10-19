5c050f46f007ed7353bc9a1b8e4e8c181aa3542d
Jacob <jacob.stemmer@student.htldornbirn.at>
2023-10-19 15:31:30 +0200

    WOOOOWOOWWW

---
+++ src/LOOPS.java
@@ -0,0 +1,13 @@
+public class LOOPS{
+
+    public static void main(String[] args) {
+        int summe = 0;
+
+        for (int zahl = 2; zahl <= 1000; zahl += 2) {
+            summe += zahl;
+        }
+
+        System.out.println("Die Summe der geraden Zahlen von 1 bis 1000 beträgt: " + summe);
+    }
+}
+
