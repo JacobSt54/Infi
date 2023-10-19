846c16a136a208e3a2aeaafbda129ba1c21b09b0
Jacob <jacob.stemmer@student.htldornbirn.at>
2023-10-19 15:47:33 +0200

    WOOOOWOOWWW

---
+++ src/ZFS.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+    public class ZFS {
+        public static void main(String[] args) {
+            Random rand = new Random();
+            int sum = 0;
+
+            while (true) {
+                int randomNumber = rand.nextInt(21) + 10; // Zufallszahl zwischen 10 und 30 generieren
+                System.out.println("Zufallszahl: " + randomNumber);
+                sum += randomNumber;
+
+                if (randomNumber == 15 || randomNumber == 25) {
+                    break;
+                }
+            }
+
+            System.out.println("Das Programm wurde beendet.");
+            System.out.println("Die Summe der vorherigen Zufallszahlen ist: " + sum);
+        }
+    }
+
+
